cce7f839085b513279dd37c6f9f4622f9f631402
AbdAllah Khodeir <abdallahkhodeir@gmail.com>
2023-07-03 19:53:07 -0600

    final commit

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from chatbot import views
+from . import views
+
 
 urlpatterns = [
-    path('', views.chatbot, name='chatbot')
+    path('', views.chatbot, name='chatbot'),
+    path('login/', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout, name='logout'),
 ]
